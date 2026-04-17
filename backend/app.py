84e4a0f7bfedb73ffb6c2650c2aa2c493159a5a3
amndzWRLD <avgsmdz@gmail.com>
2026-04-17 16:42:40 -0600

    Initial commit - Docker fullstack app

---
+++ backend/app.py
@@ -0,0 +1,33 @@
+﻿from flask import Flask, jsonify
+import mysql.connector
+import time
+
+app = Flask(__name__)
+
+def get_connection():
+    while True:
+        try:
+            conn = mysql.connector.connect(
+                host="db",
+                user="root",
+                password="root",
+                database="app_db"
+            )
+            return conn
+        except:
+            print("Waiting for DB...")
+            time.sleep(2)
+
+@app.route("/list")
+def list_data():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM datos")
+    result = cursor.fetchall()
+    return jsonify(result)
+
+@app.route("/")
+def home():
+    return "API RUNNING"
+
+app.run(host="0.0.0.0", port=5000)
---
+++ db/init.sql
@@ -0,0 +1,12 @@
+﻿CREATE DATABASE IF NOT EXISTS app_db;
+USE app_db;
+
+CREATE TABLE datos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),
+    edad INT
+);
+
+INSERT INTO datos (nombre, edad) VALUES
+('Juan', 25),
+('Maria', 30);
